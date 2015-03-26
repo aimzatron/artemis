4962a9eb82fbdc97b11a47204ec3a65ae18b037f
Aimee Maher <amaher@itriagehealth.com>
2015-03-26 11:24:34 -0600

    uuid fixes blacklist issue

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -62,6 +62,6 @@ class DiaryEntriesController < ApplicationController
     end
 
     def diary_entry_params
-      params.require(:diary_entry).permit(:entry_text)
+      params.require(:diary_entry).permit(:entry_text, :uuid)
     end
 end
